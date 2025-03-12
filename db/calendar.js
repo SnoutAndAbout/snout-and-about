25ad16623ff237a193ea6c5bdc17d1f6e2b5f13a
Kevin Smith <kevin_smith1292@yahoo.com>
2025-03-12 15:14:28 -0500

    added dummy data to updateCalendar function

--- db/calendar.js
+++ db/calendar.js
@@ -1,11 +1,11 @@
 const client = require('./client.js');
 
 
-const updateCalendar = async( eventId, userId ) => {
+const updateCalendar = async( eventName, eventId, userId ) => {
   try {
     const { rows } = await client.query(`
-      INSERT INTO calendar ( event_id, user_id )
-      VALUES (${eventId}, ${userId})
+      INSERT INTO calendar ( event_name, event_id, user_id )
+      VALUES ('${eventName}', ${eventId}, ${userId})
       RETURNING *;
     `);
     return rows[0];
@@ -14,10 +14,10 @@ const updateCalendar = async( eventId, userId ) => {
   }
 }
 
-const cancelEvent = async( eventId, userId ) => {
+const cancelEvent = async( eventName, eventId, userId ) => {
   try {
     await client.query(`
-      DELETE FROM calendar WHERE event_id=${eventId} AND user_id=${userId};
+      DELETE FROM calendar WHERE event_name= ${eventName} AND event_id=${eventId} AND user_id=${userId};
     `);
   } catch (error) {
     throw new Error(error);
