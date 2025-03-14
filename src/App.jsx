7edb9324c886da94c5cb12b588945188710c7fe2
Kevin Smith <kevin_smith1292@yahoo.com>
2025-03-14 12:01:08 -0500

    creating log in

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Navbar from "./components/Navbar";
 import City from "./components/City";
 import Registration from "./components/Registration";
 import Account from "./components/Account";
+import Login from "./components/Login"
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/city/:cityId" element={<City/>}/>
         <Route path="/registration" element={<Registration/>}/>
+        <Route path="/Login" element={<Login/>}/>
         <Route path="/account" element={<Account/>}/>
       </Routes>
     </div>
